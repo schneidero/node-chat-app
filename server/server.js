34fc122364fa47e6d662ee7c18537e490ef58bad
schneidero <desneirdero@gmail.com>
2019-11-09 09:04:44 +0100

    updated port

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 
 const publicPath = path.join(__dirname, '../public')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 var app = express();
 
